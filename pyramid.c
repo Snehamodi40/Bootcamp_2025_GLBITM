d46bdae152debfcecf85dc4a7bcb14dd6cab66e7
Sneha Modi <snehamodi40@gmail.com>
2025-03-12 23:30:51 +0530

    Add files via upload

---
+++ pyramid.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+    int i,j,n;
+    printf("enter rows");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n-i-1;j++)
+        {
+            printf(" ");
+        }
+        for(j=0;j<2*i+1;j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+}
